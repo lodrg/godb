bb75a037410d6cea87ee1b066002bfd12b1e6327
lodrg <327733082@qq.com>
2024-10-11 10:18:04 +0800

    handle conn with bufio

--- main.go
+++ main.go
@@ -24,7 +24,31 @@ func main() {
             continue
         }
         fmt.Println("New Connection happened:", conn.RemoteAddr())
-        conn.Close()
+        handleConnection(conn)
     }
 }
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("Handling connection from:", conn.RemoteAddr())
 
+	reader := bufio.NewReader(conn)
+	writer := bufio.NewWriter(conn)
+
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading:", err)
+			return
+		}
+
+		// 判断是否为 "q" 以退出
+		if line == "q\r\n" {
+			fmt.Println("Received 'q', exiting...")
+			return
+		}
+
+		fmt.Printf("Received: %s", line)
+        writer.WriteString("Echo: " + line)
+        writer.Flush()
+	}
+}

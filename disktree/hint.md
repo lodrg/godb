85427caf074a496433119957d3e0b546fbb654c7
lodrg <327733082@qq.com>
2025-08-15 11:11:35 +0300

    update test and logger

--- disktree/hint.md
+++ disktree/hint.md
@@ -41,16 +41,22 @@ metadata 的情况
 
 ## 如何理解 node 和 pager 的关系？
 首先一个 tree 有一个 pager，这点在 tree 的定义中可以明确，leafNode 和 interanlNode 的 pager 都引用的 tree 的 pager
+
 一个 pager 里面有多个 page
+
 一个 tree 里面有多个 node
+
 所以在 node 里面我们会以 pageNumber 来标记当前 node 所在的 page
+
 同时在 root 里面使用 rooPageNumber 来标记根节点所在的 page
 
 新建 tree 的时候
+
     会把 page 安排好, 新建 pager，分配一个新 page，并在这个新 page 上新建一个 leaf 作为 root
     如果是 tree 里面已经有内容了，就直接把属性归位就好
 
 插入 tree 的时候
+
     需要先根据 root 所在的 page 获取 root, 然后向 root 里面 insert，这时候会根据 root 的 node 类型进行插入
         如果是 internalNode 需要根据 key 寻址 key 的子节点所在的位置，然后根据子节点位置获得子节点，向字节点 insert （可能会递归）
         如果是 leafNode 这个时候直接插入就可以，然后就是分裂的判断
--- logger/logger.go
+++ logger/logger.go
@@ -20,10 +20,23 @@ const (
 	FATAL
 )
 
+// ANSI color codes
+const (
+	colorReset   = "\x1b[0m"
+	colorGray    = "\x1b[90m"
+	colorGreen   = "\x1b[32m"
+	colorYellow  = "\x1b[33m"
+	colorRed     = "\x1b[31m"
+	colorMagenta = "\x1b[35m"
+)
+
 type Logger struct {
-	level  Level
-	logger *log.Logger
-	mu     sync.Mutex
+	level       Level
+	logger      *log.Logger
+	mu          sync.Mutex
+	levelWidth  int
+	callerWidth int
+	useColor    bool
 }
 
 var (
@@ -36,7 +49,10 @@ func getLogger() *Logger {
 		defaultLogger = &Logger{
 			level: DEBUG,
 			// 移除 log.Lshortfile，我们自己处理文件信息
-			logger: log.New(os.Stdout, "", log.LstdFlags),
+			logger:      log.New(os.Stdout, "", log.LstdFlags),
+			levelWidth:  5,  // 固定列宽: [LEVEL]
+			callerWidth: 25, // 固定列宽: 文件:行号
+			useColor:    true,
 		}
 	})
 	return defaultLogger
@@ -49,13 +65,67 @@ func SetLevel(level Level) {
 	l.level = level
 }
 
+// 开关: 是否启用彩色输出
+func SetColorEnabled(enabled bool) {
+	l := getLogger()
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.useColor = enabled
+}
+
+// 设置对齐列宽: levelWidth 为级别列宽, callerWidth 为调用点(文件:行号)列宽
+// 当实际文本长度超过设置的列宽时，将右对齐保留末尾，并在需要时裁剪左侧
+func SetColumnWidths(levelWidth, callerWidth int) {
+	l := getLogger()
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	if levelWidth > 0 {
+		l.levelWidth = levelWidth
+	}
+	if callerWidth > 0 {
+		l.callerWidth = callerWidth
+	}
+}
+
 // 获取调用者的文件和行号
 func getCaller(skip int) string {
 	_, file, line, ok := runtime.Caller(skip)
 	if !ok {
 		return "???"
 	}
+	// 使用相对路径用于IDE跳转
 	return fmt.Sprintf("%s:%d", filepath.Base(file), line)
+}
+
+func fitWidthLeftAlign(s string, width int) string {
+	if width <= 0 {
+		return s
+	}
+	runes := []rune(s)
+	if len(runes) > width {
+		return string(runes[:width])
+	}
+	return fmt.Sprintf("%-*s", width, s)
+}
+
+func colorize(level Level, text string, useColor bool) string {
+	if !useColor {
+		return text
+	}
+	switch level {
+	case DEBUG:
+		return colorGray + text + colorReset
+	case INFO:
+		return colorGreen + text + colorReset
+	case WARN:
+		return colorYellow + text + colorReset
+	case ERROR:
+		return colorRed + text + colorReset
+	case FATAL:
+		return colorMagenta + text + colorReset
+	default:
+		return text
+	}
 }
 
 func output(calldepth int, level Level, format string, v ...interface{}) {
@@ -82,10 +152,13 @@ func output(calldepth int, level Level, format string, v ...interface{}) {
 		levelStr = "FATAL"
 	}
 
-	// 格式: [级别] 文件:行号 消息
+	// 列: [LEVEL] 调用点 消息
 	caller := getCaller(calldepth)
-	// 格式: [级别] 文件名:行号 消息
-	logMsg := fmt.Sprintf("[%s] %s %s", levelStr, caller, msg)
+	paddedLevel := fmt.Sprintf("%-*s", l.levelWidth, levelStr)
+	coloredLevel := colorize(level, paddedLevel, l.useColor)
+	callerCol := fitWidthLeftAlign(caller, l.callerWidth)
+
+	logMsg := fmt.Sprintf("[%s] %s %s", coloredLevel, callerCol, msg)
 	l.logger.Output(2, logMsg)
 
 	if level == FATAL {
